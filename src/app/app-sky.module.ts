e3d03b03ad0ce2c82fe837dbfbb8611e494ba8dd
Michael Tims <michaeltims81@gmail.com>
2019-05-17 12:32:45 -0400

    Confirm and error dialogs, flyout add-in (#3)

--- src/app/app-sky.module.ts
+++ src/app/app-sky.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
+import { SkyAvatarModule } from '@skyux/avatar';
 import { SkyCheckboxModule } from '@skyux/forms';
 import { SkyI18nModule } from '@skyux/i18n';
-import { SkyWaitModule, SkyAlertModule } from '@skyux/indicators';
+import { SkyWaitModule, SkyAlertModule, SkyKeyInfoModule, SkyLabelModule } from '@skyux/indicators';
+import { SkyPageSummaryModule } from '@skyux/layout';
+import { SkyListModule } from '@skyux/list-builder';
+import { SkyListViewGridModule } from '@skyux/list-builder-view-grids';
 import { SkyModalModule } from '@skyux/modals';
 
 @NgModule({
   exports: [
     SkyAlertModule,
+    SkyAvatarModule,
     SkyCheckboxModule,
     SkyI18nModule,
+    SkyKeyInfoModule,
+    SkyLabelModule,
+    SkyListModule,
+    SkyListViewGridModule,
     SkyModalModule,
+    SkyPageSummaryModule,
     SkyWaitModule
   ]
 })
